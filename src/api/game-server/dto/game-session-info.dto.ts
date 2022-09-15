46ae0566da63a7ed497d5dca07fc4a4effc559c2
serhattanrikut <serhat@wodo.io>
2022-09-15 09:00:51 +0000

    game session info with url added to the dtos

---
+++ src/api/game-server/dto/game-session-info.dto.ts
@@ -0,0 +1,3 @@
+export class GameSessionInfoDto {
+    url:string;
+}
--- src/api/game-server/game-server.controller.ts
+++ src/api/game-server/game-server.controller.ts
@@ -1,5 +1,6 @@
 import { Get, Post, Body, Put, Delete, Param, Query, Controller, ParseIntPipe } from '@nestjs/common';
 import { ApiParam, ApiBearerAuth, ApiResponse, ApiOperation, ApiTags, ApiQuery, } from '@nestjs/swagger';
+import { GameSessionInfoDto } from './dto/game-session-info.dto';
 import { GameSessionDto } from './dto/game-session.dto';
 
 
@@ -11,11 +12,14 @@ export class GameServerController {
 
 
   @ApiOperation({ summary: 'Initiate game session on a remote game server.' })
-  @ApiResponse({ status: 201, description: 'The gmae session data has been successfully sent.' })
+  @ApiResponse({ status: 201, description: 'The game session data has been successfully initiated.' , type:GameSessionInfoDto})
   @ApiResponse({ status: 403, description: 'Forbidden.' })
   //@UsePipes(new ValidationPipe())
   @Post()
-  async initiateGmaeSession(@Body() gameResult:GameSessionDto):Promise<void> {
+  async initiateGmaeSession(@Body() session:GameSessionDto):Promise<GameSessionInfoDto> {
+    return Promise.resolve({
+      url: "https://wodoland.io/games/sessions/1"
+    })
   }
 
 }
